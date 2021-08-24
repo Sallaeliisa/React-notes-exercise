d49c3aae4dc4b4c82966b737db2d0d600167ce6b
Vuorikko Salla <salla.vuorikko@gmail.com>
2021-08-24 10:53:40 +0300

    items can be deleted from the list

--- src/Components/Notes.js
+++ src/Components/Notes.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 const Notes = () => {
   const [notes, setNotes] = useState([]);
@@ -9,32 +9,54 @@ const Notes = () => {
   const addNote = (e) => {
     e.preventDefault();
     const value = document.getElementById("addNote").value;
+    const newNote = { id: new Date(), content: value, completed: false };
     if (value.length > 0) {
-      setNotes([...notes, value]);
+      setNotes([...notes, newNote]);
     }
     document.getElementById("addNote").value = "";
   };
 
-  const done = () => {
-    console.log('done');
-  }
+  const remove = (task) => {
+    const selectedTask = { ...task };
+    const updatedNotes = notes.filter((item) => item.id !== selectedTask.id);
+    setNotes(updatedNotes);
+  };
+
+  const isComplite = (task) => {
+    const checkTask = { ...task };
+    const checkedNotes = notes;
+    checkedNotes.forEach((item) => {
+      if (item.id === checkTask.id) {
+        item.completed = !checkTask.completed;
+      }
+    });
+    setNotes(checkedNotes);
+    console.log(checkedNotes);
+  };
 
   return (
     <div>
-        <header>
-            <h1>Notes to myself</h1>
-            <p>I have completed 0/{notes.length} notes</p>
-        </header>
+      <header>
+        <h1>Notes to myself</h1>
+        <p>Number of notes: {notes.length}</p>
+      </header>
       <form>
         <input type="text" id="addNote"></input>
         <button onClick={addNote}>Add note</button>
       </form>
       <section>
         <ul>
-          {notes.map((el) => {
-            return <li key={notes.indexOf(el)}>{el}
-            <button><FontAwesomeIcon icon={faTrash} /></button></li>;
-          })}
+          {notes.map((task) => (
+            <li
+              key={task.id}
+              className={task.completed ? "taskComplete" : "taskIncomplete"}
+            >
+              <label onClick={() => isComplite(task)}>{task.content}</label>
+              <button onClick={() => remove(task)}>
+                <FontAwesomeIcon icon={faTrash} />
+              </button>
+            </li>
+          ))}
         </ul>
       </section>
     </div>
